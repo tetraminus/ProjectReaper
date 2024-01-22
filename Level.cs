67a3e81f2b58fb23c0213444210390b03e965181
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-01-21 23:16:01 -0800

    art stuff

---
+++ Level.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+using ProjectReaper.Util;
+using LevelGenerator = ProjectReaper.Globals.LevelGenerator;
+
+public partial class Level : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GameManager.Level = this;
+		GD.Randomize();
+		LevelGenerator.Instance.GenerateLevel(RoomSetLoader.LoadRoomSet("TestRooms"), 5, 5, GD.Randi());
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
