69471cab1dde948db5a894e276d43030629ac151
ilikepurper <148371815+ilikepurper@users.noreply.github.com>
2024-01-25 10:21:26 -0800

    slimbert stuff

--- Enemies/Slimebert.cs
+++ Enemies/Slimebert.cs
@@ -3,6 +3,8 @@ using ProjectReaper.Abilities;
 using ProjectReaper.Util;
 using System;
 using ProjectReaper.Enemies;
+using System.Collections.Specialized;
+using System.Diagnostics;
 
 public partial class Slimebert : AbstractCreature
 {
@@ -21,6 +23,18 @@ public partial class Slimebert : AbstractCreature
 
 	private Timer ShootTimer;
 
+
+    private Random random = new Random();
+    private Vector2 randomDirection = Vector2.Zero; // Store the random direction
+    ulong lastTimeMs = 0;
+
+    float movementDist = 50.0f;
+
+
+
+
+    
+
     public override void _Ready()
     {
         stats.Init();
@@ -33,6 +47,12 @@ public partial class Slimebert : AbstractCreature
         stateTimer.Timeout += _OnStateTimerTimeout;
 		stateTimer.Start();
 
+
+        randomDirection.X = (2.0f - (float)GD.RandRange(0.0f, 1.0f)) * movementDist;
+        randomDirection.Y = (2.0f - (float)GD.RandRange(0.0f, 1.0f)) * movementDist;
+        randomDirection.X += GlobalPosition.X;
+        randomDirection.Y += GlobalPosition.Y;
+        
 
 
 		        // Create and add a timer for state switching
@@ -60,7 +80,7 @@ public partial class Slimebert : AbstractCreature
         switch (currentState)
         {
             case EnemyState.Moving:
-                MoveTowardsPlayer();
+                MoveRandomly(randomDirection * stats.Speed);
                 break;
             case EnemyState.Shooting:
                 ShootState();
@@ -88,6 +108,42 @@ public partial class Slimebert : AbstractCreature
             MoveAndSlide();
         }
     }
+    
+    
+    private void MoveRandomly(Vector2 vector2)
+    {
+        // Generate a random direction
+        //Vector2 randomDirection = new Vector2((float)GD.Randf() * 2 - 1, (float)GD.Randf() * 2 - 1).Normalized();
+        ulong currentTimeMs = Time.GetTicksMsec();       
+
+        //if ((currentTimeMs - lastTimeMs) > 20.0) {
+        Debug.Print(randomDirection.ToString());
+
+        if (GlobalPosition.DistanceSquaredTo(randomDirection) < 5.0f) {
+            randomDirection.X = (2.0f - (float)GD.RandRange(0.0f, 1.0f)) * movementDist;
+            randomDirection.Y = (2.0f - (float)GD.RandRange(0.0f, 1.0f)) * movementDist;
+            if ((float)GD.RandRange(0.0f, 1.0f) > 0.5f) {
+                randomDirection.X *= -1.0f;
+            }
+            if ((float)GD.RandRange(0.0f, 1.0f) > 0.5f) {
+                randomDirection.Y *= -1.0f;
+            }
+            randomDirection.X += GlobalPosition.X;
+            randomDirection.Y += GlobalPosition.Y;
+        }
+
+        GlobalPosition = GlobalPosition.Lerp(randomDirection, 0.05f);
+        
+
+        // Move the enemy in the random direction
+        //Velocity = randomDirection * stats.Speed;
+        //MoveAndSlide();
+    }
+
+
+
+
+
 
     private void ShootState()
     {
