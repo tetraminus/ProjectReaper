26196869c2d85d4116e1c4ddb61b91fba883ca0f
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-02-27 18:40:20 -0800

    begin forcefield

---
+++ Items/Collectables/ForceField.cs
@@ -0,0 +1,18 @@
+using System;
+using Godot;
+using ProjectReaper.Enemies;
+using ProjectReaper.Globals;
+
+namespace ProjectReaper.Items.Collectables;
+
+public partial class ForceField : AbstractItem {
+    public override string Id => "force_field";
+
+
+    public override void OnInitalPickup() {
+        
+    }
+
+
+   
+}
