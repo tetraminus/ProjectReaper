8c02f22e0c869307066e485902034b59371ef26b
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-02-29 16:31:19 -0800

    arts

--- Items/Collectables/DreamCar.cs
+++ Items/Collectables/DreamCar.cs
@@ -5,9 +5,21 @@ namespace ProjectReaper.Items.Collectables;
 public partial class DreamCar : AbstractItem
 {
     public override string Id => "dream_car";
-    
+    private const float CooldownReduction = 0.1f; // 10% per stack
+    private float currentReduction = 0f;
     public override void OnInitalPickup()
     {
-        GameManager.Player.GetAbility(3).Cooldown = 0;
+        base.OnInitalPickup();
+        var originalCooldown = GameManager.Player.GetAbility(3).Cooldown;
+        currentReduction = originalCooldown * CooldownReduction * Stacks;
+        GameManager.Player.GetAbility(3).Cooldown -= currentReduction;
+    }
+
+    public override void OnStack(int newstacks) {
+        base.OnStack(newstacks);
+        GameManager.Player.GetAbility(3).Cooldown += currentReduction;
+        var originalCooldown = GameManager.Player.GetAbility(3).Cooldown;
+        currentReduction = originalCooldown * CooldownReduction * Stacks;
+        GameManager.Player.GetAbility(3).Cooldown -= currentReduction;
     }
 }
