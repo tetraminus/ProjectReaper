6b8b82db1ddc661001eeda429fe6f3f668e706fb
AidancodeStudios <89610708+AidancodeStudios@users.noreply.github.com>
2024-03-26 12:01:50 -0700

    tileset added

--- Abilities/SoldierShoot.cs
+++ Abilities/SoldierShoot.cs
@@ -7,20 +7,20 @@ namespace ProjectReaper.Abilities;
 
 public partial class SoldierShoot : AbstractAbility
 {
-    private static PackedScene BulletScene { get; } =
-        GD.Load<PackedScene>("res://Abilities/Projectiles/BasicBullet.tscn");
+	private static PackedScene BulletScene { get; } =
+		GD.Load<PackedScene>("res://Abilities/Projectiles/BasicBullet.tscn");
 
-    public override float Cooldown { get; set; } = 0.2f;
-    
+	public override float Cooldown { get; set; } = 0.2f;
+	
 
-    public override void Use()
-    {
-        var bullet = (AbstractDamageArea)BulletScene.Instantiate();
-        Callbacks.Instance.EmitSignal(Callbacks.SignalName.BulletCreated, bullet);
-        var src = GameManager.Player;
-        bullet.Init(src, src.Team, src.GlobalPosition,src.AimDirection());
-        
-        GetTree().Root.AddChild(bullet);
-        
-    }
+	public override void Use()
+	{
+		var bullet = (AbstractDamageArea)BulletScene.Instantiate();
+		Callbacks.Instance.EmitSignal(Callbacks.SignalName.BulletCreated, bullet);
+		var src = GameManager.Player;
+		bullet.Init(src, src.Team, src.GlobalPosition,src.AimDirection());
+		
+		GetTree().Root.AddChild(bullet);
+		
+	}
 }
