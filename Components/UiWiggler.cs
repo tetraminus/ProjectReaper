640725a063795d3af39a80b4c3506e9d5363a367
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-04-10 22:51:17 -0700

    logo physics

--- Components/UiWiggler.cs
+++ Components/UiWiggler.cs
@@ -7,8 +7,10 @@ public partial class UiWiggler : Node2D
 	private Vector2 _initialPosition;
 	private float initialRotation;
 	private Vector2 _velocity;
+	private float _rotationVelocity;
 	[Export] public float gravity = 1f;
 	[Export] public float turbulence = 50f;
+	[Export] public float mouseInfluence = 0.1f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -21,17 +23,26 @@ public partial class UiWiggler : Node2D
 	public override void _Process(double delta)
 	{
 		_velocity +=  (new Vector2(0, 1)).Rotated((float) GD.RandRange(0, Mathf.Pi * 2)) * (float)delta * turbulence;
+		_rotationVelocity += (float) GD.RandRange(-1, 1) * (float)delta * turbulence/10f;
 		
+		// mouse influence
+		var mousePos = GetGlobalMousePosition();
+		var dir = mousePos - GlobalPosition;
+		var distance = dir.Length();
 		
+		_velocity += dir.Normalized() * mouseInfluence * (float) delta * Mathf.Clamp(1 - distance / 500, 0, 1);
+		
+		// move
 		Position += _velocity * (float) delta;
+		Rotation += _rotationVelocity * (float) delta;
 		
 		_velocity = _velocity.Lerp(Vector2.Zero, 0.001f * (float) delta);
 		// gravity based on distance and velocity 
 		
 		
-		_velocity = _velocity.Lerp(_initialPosition - Position, gravity * (float) delta );
+		_velocity = _velocity.Lerp(_initialPosition - Position, gravity * (float) delta * (Position.DistanceTo(_initialPosition) / 10));
+		_rotationVelocity = Mathf.Lerp(_rotationVelocity,  initialRotation -Rotation , gravity * (float) delta);
 		
-		Rotation = _velocity.Angle();
 		
 	}
 	
